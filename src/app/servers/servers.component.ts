74742dbc457b16d4b288ca8a519f01363f83689e
alperbasak <aalperbasak@gmail.com>
2021-08-17 10:23:26 +0200

    navigating programmatically with relativeTo option

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ServersService } from './servers.service';
+import {Component, OnInit} from '@angular/core';
+import {ServersService} from './servers.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -7,12 +8,21 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number, name: string, status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  // 'relativeTo' default value is '/', so it will always try to navigate to an absolute path
+  // reading active route metadata from ActivatedRoute and give it to relativeTo option to navigate relatively
+  onReload() {
+    // this will try to go to /servers/servers and will fail
+    this.router.navigate(['servers'], {relativeTo: this.route});
+  }
 }
