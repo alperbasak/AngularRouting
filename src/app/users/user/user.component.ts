1a89580aa34501474f0396063fd5b3346ff7827d
alperbasak <aalperbasak@gmail.com>
2021-08-17 11:08:00 +0200

    Unsubscribing route params observable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,12 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+  paramsSubscription: Subscription;
+
   user: { id: number, name: string };
 
   constructor(private route: ActivatedRoute) {
@@ -18,7 +21,8 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params.name
     };
 
-    this.route.params.subscribe(
+    // Observable subscribing for dynamic template updates
+    this.paramsSubscription = this.route.params.subscribe(
       (params) => {
         this.user.id = params.id;
         this.user.name = params.name;
@@ -26,4 +30,9 @@ export class UserComponent implements OnInit {
     );
   }
 
+  ngOnDestroy(): void {
+    // normally once the component is disposed, angular unsubs automatically.
+    // However for custom Observables, you might wanna unsub specifically
+    this.paramsSubscription.unsubscribe();
+  }
 }
