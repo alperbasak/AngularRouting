d33bc9d21264718223f57ef2ab92db2c02f847de
alperbasak <aalperbasak@gmail.com>
2021-08-17 11:03:05 +0200

    adding parameters to routes dynamically and updating the view with Params observable on route change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
   // ! not /users
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: ServersComponent}
 ];
 
